cc37659fafecaf86694d5b3c877c039e8f9cc387
Fareed Rezaei <fareed9@msn.com>
2023-07-22 17:37:42 +0100

    added missing library

--- src/qetaa/jsf/helpers/AppConstants.java
+++ src/qetaa/jsf/helpers/AppConstants.java
@@ -6,7 +6,7 @@ public class AppConstants {
 	public final static String GIFTS_DIRECTORY_LINUX = "/home/ubuntu/loyalty-gifts-pics/";
 	public final static String VIN_DIRECTORY_WINDOWS = "C:\\VIN-DIR\\";
 	public final static String VIN_DIRECTORY_LINUX = "/home/ubuntu/VIN-DIR/";
-	public final static String SERVICE_HOST = "http://localhost:8081";
+	public final static String SERVICE_HOST = "http://localhost:8080";
 	public final static String APP_SECRET = "093X3b*y&iWu4U&F181X#3ZE0)%9374";
 	private final static String OS = System.getProperty("os.name").toLowerCase();
 	
